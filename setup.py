60bd40f987f38af46b728f4b4fd3d106c08a4e8b
Nyiema Bayfield <ntmbayfield@gmail.com>
2018-09-13 15:34:26 -0700

    Create mainfest folder, dist folder and __.init.py__ file in k911-python-alerts-server directory.

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='K911PythonAlertsServer',
+    version='0.1',
+    packages=['k911-python-alerts-server',],
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    long_description=open('README.txt').read(),
+)
